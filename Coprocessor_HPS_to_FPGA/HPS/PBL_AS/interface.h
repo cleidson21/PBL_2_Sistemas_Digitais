5574462ef3c96f7a65e6b7a0eb543879902fec7f
Cleidson Ramos <cleidson.ramos788@gmail.com>
2025-05-22 15:15:57 -0300

    Update interface.h

--- Coprocessor_HPS_to_FPGA/HPS/PBL_AS/interface.h
+++ Coprocessor_HPS_to_FPGA/HPS/PBL_AS/interface.h
@@ -8,21 +8,6 @@
 #define HW_SEND_FAIL   -2
 #define HW_READ_FAIL   -3
 
-/* ========== DEFINIÇÕES DE HARDWARE ========== */
-#define DATA_IN_BASE    0x0
-#define DATA_OUT_BASE   0x10
-#define LW_BRIDGE_BASE  0xFF200000
-#define LW_BRIDGE_SPAN  0x00005000
-
-/* ========== BITS DE CONTROLE ========== */
-#define OPCODE_BITS     (3 << 16)
-#define SIZE_BITS       (3 << 19)
-#define SCALAR_BITS     (3 << 21)
-#define RESET_BIT       (1 << 29)
-#define START_PULSE_BIT (1 << 30)
-#define HPS_CONTROL_BIT (1 << 31)
-#define FPGA_ACK_BIT    (1 << 31)
-
 /* ========== ESTRUTURAS DE DADOS ========== */
 struct Params {
     const int8_t* a;
